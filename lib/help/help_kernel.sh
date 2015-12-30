2e3e52515fa983f09beda29e1f244d959fcbcf13
Pavel Stratil <pavel@killua.eu>
2015-12-30 20:50:16 +0100

    Additions to mage kernel helpfile, @default linuxconfig file

--- lib/help/help_kernel.sh
+++ lib/help/help_kernel.sh
@@ -3,8 +3,13 @@
 
 help_kernel() {
 cat << HELP_END
-${BAD}mage kernel${NORMAL} - no help available yet
- 
+${BAD}mage kernel${NORMAL} - a helper to compile and install linux kernels.
+
+ * ${HILITE}mage kernel make${NORMAL}
+ * ${HILITE}mage kernel install${NORMAL}
+
+See also mage help linuxconfig for the ${HILITE}mage linuxconfig${NORMAL}, a linux kernel configuration helper. 
+
 HELP_END
 exit ${1:-1}
 }
