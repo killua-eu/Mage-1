5ef4f5533a4a58df942e7d21317c88931ca7006c
Pavel Stratil <pavel@killua.eu>
2015-12-30 22:43:27 +0100

    Playing with lxd

---
+++ profiles/app/svc-virt/todo.brsetup.sh
@@ -0,0 +1,15 @@
+# script to setup a natted network for lxc guests
+CMD_BRCTL="brctl"
+CMD_IFCONFIG="ifconfig"
+CMD_IPTABLES=/sbin/iptables
+CMD_ROUTE=/sbin/route
+NETWORK_BRIDGE_DEVICE_NAT=lxcbr0
+HOST_NETDEVICE=wlp2s0
+PRIVATE_GW_NAT=192.168.100.1
+PRIVATE_NETMASK=255.255.255.0
+
+${CMD_BRCTL} addbr ${NETWORK_BRIDGE_DEVICE_NAT}
+${CMD_BRCTL} setfd ${NETWORK_BRIDGE_DEVICE_NAT} 0
+${CMD_IFCONFIG} ${NETWORK_BRIDGE_DEVICE_NAT} ${PRIVATE_GW_NAT} netmask ${PRIVATE_NETMASK} promisc up
+${CMD_IPTABLES} -t nat -A POSTROUTING -o ${HOST_NETDEVICE} -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
